0bbf4ac595160e524df43ab119767f69bdb6133a
“yingzhang0897” <yinganticpc@gmail.com>
2024-05-03 13:50:49 -0700

    events handling added

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,25 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const output = document.querySelector('ul');
-const list = document.createElement('li');
-list.textContent = input.value;
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+li.textContent = input.value;
 const deleteButton = document.createElement('deleteButton');
 deleteButton.textContent = 'X';
-list.append(deleteButton);
-ul.append(list);
+li.append(deleteButton);
+list.append(li);
+
+//event handling
+button.addEventListener('click',function(){
+    if (input.value.trim() !== ' ' ) {
+        list.appendChild(li);
+    } else {
+        alert("Blank input! Please try again!");
+        input.focus();
+    }
+});
+deleteButton.addEventListener('click',function(){
+    list.removeChild(li);
+    input.focus();
+});
+input.value = '';
+input.focus();
